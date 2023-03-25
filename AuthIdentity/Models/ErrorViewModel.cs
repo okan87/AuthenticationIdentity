af1c45577e09ad888738a1235ab639f2c0f41973
okan87 <okankaplan1907@gmail.com>
2023-03-25 11:17:31 +0100

    Auth.Identity is finished

---
+++ AuthIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
